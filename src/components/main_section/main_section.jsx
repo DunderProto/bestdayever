7f8317a3245439669a478fd472d11669fda1c8a6
Isaac Nam <ikn208@nyu.edu>
2021-04-24 19:41:55 -0400

    added the main section and styling

--- src/components/main_section/main_section.jsx
+++ src/components/main_section/main_section.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './main_section.css';
 import BingoContainer from '../bingo_container/bingo_container.jsx';
 import '../../index.css';
 
@@ -10,7 +11,17 @@ class MainSection extends React.Component {
     render() {
         return (
             <>
-                <div className="main_section flex">
+                <div className="main_section">
+                    <div className="main_section_header">
+                        <h1>Welcome,</h1>
+                        <h1>Isaac Nam</h1>
+                    </div>
+                    <div className="main_section_description">
+                        <p>Below is your bingo board. As actions occur during the event,
+                            you will see the related boxes marked off. When you get "X" in
+                            a row, you can call BINGO!
+                        </p>
+                    </div>
                     <BingoContainer />
                 </div>
             </>
