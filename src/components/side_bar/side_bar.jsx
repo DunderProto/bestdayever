3d45b2199460dea7d006261f0bd4d5de51b1911e
Isaac Nam <ikn208@nyu.edu>
2021-04-25 13:28:20 -0400

    added BDE logos to sidebar

--- src/components/side_bar/side_bar.jsx
+++ src/components/side_bar/side_bar.jsx
@@ -11,9 +11,16 @@ class SideBar extends React.Component {
         return (
             <>
                 <div className="side_bar">
-                    <div className="side_bar_logo"></div>
-                    <div className="side_bar_welcome"></div>
-                    <div className="bottom_image"></div>
+                    <div className="side_bar_logo_container">
+                        <img className="side_bar_logo" src="https://image-test-ssk.s3.amazonaws.com/BDE.png" alt=""/>
+                    </div>
+                    <div className="side_bar_welcome flex">
+                        <p className="welcome_text">Welcome to</p>
+                        <h1 className="welcome_event">Football Masters</h1>
+                    </div>
+                    <div className="bottom_image">
+                        <img className="side_bar_logo_bottom" src="https://image-test-ssk.s3.amazonaws.com/BDE.png" alt=""/>
+                    </div>
                 </div>
             </>
         )
